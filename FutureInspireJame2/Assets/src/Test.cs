69cea990fc7dc1ffaa28481eee3c375f0975e070
GeorgePerakis <143959106+GeorgePerakis@users.noreply.github.com>
2023-09-05 21:41:30 +0300

    Update Test.cs

--- FutureInspireJame2/Assets/src/Test.cs
+++ FutureInspireJame2/Assets/src/Test.cs
@@ -7,8 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("this is a test");
-
+        Debug.Log("this is a test2");
     }
 
     // Update is called once per frame
