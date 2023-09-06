015cbf68e2655fe6fb3f5280cdb9ac450f58561e
RaykoEZ <onion55zh@gmail.com>
2023-09-06 12:46:37 +0100

    Finished Player Base spawning projectiles within range
    Added a ranged spawner prefab for any scripted gameobject

--- FutureInspireJame2/Assets/src/PlayerHomeBase.cs
+++ FutureInspireJame2/Assets/src/PlayerHomeBase.cs
@@ -8,7 +8,7 @@ public class PlayerHomeBase : MonoBehaviour
     [SerializeField] float m_projectileSpawnInterval = default;
     [SerializeField] int m_spawnLimit = default;
     [SerializeField] DraggableProjectile m_toSpawn = default;
-    [SerializeField] float m_spawnRadius = default;
+    [SerializeField] RangedSpawner m_spawner = default;
     int m_projectilesSpawned = 0;
     // Start is called before the first frame update
     void Start()
@@ -20,13 +20,10 @@ public class PlayerHomeBase : MonoBehaviour
     {
         while (m_projectilesSpawned < m_spawnLimit) 
         {
-            // sample a random position around home base
-            float randomRadius = Random.Range(2f, m_spawnRadius);
-            Vector3 randomSpawnPos = Random.insideUnitCircle * randomRadius;
-            // spawn projectile at a random position within [m_spawnRadius]
-            Spawner.SpawnObject(m_toSpawn, transform.position + randomSpawnPos, transform.parent);
-            m_projectilesSpawned++;
             yield return new WaitForSeconds(m_projectileSpawnInterval);
+            // spawn projectile at a random position within [m_spawnRadius]
+            m_spawner?.Spawn(m_toSpawn);
+            m_projectilesSpawned++;
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
---
+++ FutureInspireJame2/Assets/src/RadialRangeRenderer.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Curry.Util;
+
+namespace Curry.Game
+{
+    // Displays circular/polygon range with polygon collider 2D
+    [RequireComponent(typeof(MeshFilter))]
+    [RequireComponent(typeof(MeshRenderer))]
+    public class RadialRangeRenderer : MonoBehaviour
+    {
+        // How many sides to generate for the circle/polygon
+        [Range(1, 360)]
+        [SerializeField] protected int m_rayCount = default;
+        // Angle of range to display
+        [Range(0, 360f)]
+        [SerializeField] protected float m_fov = default;
+        // radius of range
+        [SerializeField] protected float m_viewRadius = default;
+        [SerializeField] protected MeshFilter m_meshFilter = default;
+        [SerializeField] protected PolygonCollider2D m_collider = default;
+        [SerializeField] protected LayerMask m_raycastMask = default;
+        protected List<Vector3> m_verts;
+        protected List<int> m_triangles;
+        protected Mesh m_renderMesh;
+        protected float m_angleInterval;
+        protected float m_faceAngle = 0f;
+        public float Radius { get { return m_viewRadius; } set { m_viewRadius = value; } }
+        public float FieldOfViewDegree { get { return m_fov; } set { m_fov = value; } }
+
+        public void FaceTowards(Vector2 dir) 
+        {
+            m_faceAngle = VectorExtension.DegreeFromDirection(dir);
+        }
+
+        protected virtual void Awake() 
+        {
+            //Setup mesh and array sizes
+            m_renderMesh = new Mesh();
+            m_renderMesh.name = "RangeMesh";
+            m_meshFilter.mesh = m_renderMesh;
+            m_angleInterval = m_fov / m_rayCount;
+            m_verts = new List<Vector3>(new Vector3[m_rayCount + 2]);
+            m_verts[0] = Vector3.zero;
+            m_triangles = new List<int>(new int[m_rayCount * 3]);
+        }
+
+        protected virtual void LateUpdate()
+        {
+            RenderMesh();
+        }
+
+        protected virtual void RenderMesh() 
+        {
+            if(Mathf.Approximately(m_viewRadius, 0f)) 
+            {
+                return;
+            }
+
+            SetVisibleExtent();
+            SetTriangleIndex();
+
+            m_renderMesh.Clear();
+            m_renderMesh.vertices = m_verts.ToArray();
+            m_renderMesh.triangles = m_triangles.ToArray();
+            m_renderMesh.RecalculateNormals();
+
+            Vector2[] points = VectorExtension.ToVector2Array(m_verts.ToArray());
+            m_collider.points = points;
+        }
+
+        protected virtual void SetVisibleExtent() 
+        {
+            float angle = m_faceAngle;
+            // Set visible extent for mesh with raycasts
+            for (int i = 0; i < m_verts.Count - 1; ++i)
+            {
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, VectorExtension.VectorFromDegree(angle), m_viewRadius, m_raycastMask);                   
+                // Check if there are obstacles
+                if (hit.collider != null)
+                {
+                    m_verts[i + 1] = transform.InverseTransformPoint(hit.point);
+                }
+                else
+                {
+                    Vector3 localPoint = VectorExtension.VectorFromDegree(angle) * m_viewRadius;
+                    m_verts[i + 1] = localPoint;
+
+                }
+                angle -= m_angleInterval;
+            }
+        }
+
+        protected virtual void SetTriangleIndex() 
+        {
+            // assign triangle order
+            int v = 0;
+            for (int i = 0; i < m_triangles.Count; i += 3)
+            {
+                m_triangles[i] = 0;
+                m_triangles[i + 1] = v + 1;
+                m_triangles[i + 2] = v + 2;
+                ++v;
+            }
+        }
+    }
+}
