ed1ba272ef96bd5a551dce88adcdcd7af7ed92f4
RaykoEZ <onion55zh@gmail.com>
2023-09-07 15:31:52 +0100

    Adding enemy wave spawning data

--- FutureInspireJame2/Assets/src/EnemyWaveManager.cs
+++ FutureInspireJame2/Assets/src/EnemyWaveManager.cs
@@ -5,12 +5,16 @@ public class EnemyWaveManager : MonoBehaviour
     [SerializeField] EnemySpawner m_spawner = default;
     [SerializeField] Enemy m_spawnRef = default;
     [SerializeField] PlayerHomeBase m_playerBase;
+    float m_waveTimer = 0f;
     // Start is called before the first frame update
     void Start()
     {
         m_spawner?.SpawnEnemies(m_spawnRef, 5, 0.5f, InitEnenmy);
     }
-
+    void StartWave() 
+    { 
+    
+    }
     void InitEnenmy(Enemy spawned) 
     {
         spawned.Init(m_playerBase.transform);
