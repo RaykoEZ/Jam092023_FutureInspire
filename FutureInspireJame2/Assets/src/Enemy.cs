6115ec235cc2208443ed4df2b4d5dc81645c7859
GeorgePerakis <giorgosperakis100@gmail.com>
2023-09-10 22:37:43 +0300

    Added FX after timeline ends

--- FutureInspireJame2/Assets/src/Enemy.cs
+++ FutureInspireJame2/Assets/src/Enemy.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Playables;
 public delegate void OnEnemyDefeat(Enemy defeated);
 [RequireComponent(typeof(Rigidbody2D))]
 public class Enemy : MonoBehaviour, IPushable
@@ -10,7 +11,14 @@ public class Enemy : MonoBehaviour, IPushable
     Coroutine m_movement;
     Transform m_target;
     public event OnEnemyDefeat OnDefeat;
-    public void Init(Transform target) 
+    private AudioSource audioSource;
+    private PlayableDirector playableDirector;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        playableDirector = GetComponent<PlayableDirector>();
+    }
+    public void Init(Transform target)
     {
         m_target = target;
         StartMoving();
@@ -19,21 +27,24 @@ public class Enemy : MonoBehaviour, IPushable
     {
         StartCoroutine(Push_Internal(dirNormalize, power));
     }
-    public void OnDefeated() 
+    public void OnDefeated()
     {
         StopMoving();
-        OnDefeat?.Invoke(this);
-        Destroy(gameObject);
+        if (playableDirector != null)
+        {
+            playableDirector.Play();
+        }
+        StartCoroutine(DestroyAfterTimeline());
     }
 
-    public void StartMoving() 
+    public void StartMoving()
     {
-        if(m_movement == null && m_target != null) 
+        if (m_movement == null && m_target != null)
         {
             m_movement = StartCoroutine(Movement());
         }
     }
-    public void StopMoving() 
+    public void StopMoving()
     {
         if (m_movement != null)
         {
@@ -41,18 +52,18 @@ public class Enemy : MonoBehaviour, IPushable
             m_movement = null;
         }
     }
-    IEnumerator Push_Internal(Vector2 dirNormalize, float power) 
+    IEnumerator Push_Internal(Vector2 dirNormalize, float power)
     {
         StopMoving();
         GetComponent<Rigidbody2D>()?.AddForce(dirNormalize * power, ForceMode2D.Impulse);
         yield return new WaitForSeconds(1f);
         StartMoving();
     }
-    IEnumerator Movement() 
+    IEnumerator Movement()
     {
         float dist = Vector3.Distance(transform.position, m_target.position);
         float t = 0f;
-        while (dist > 0.01f) 
+        while (dist > 0.01f)
         {
             t += m_moveInterval * Time.deltaTime;
             transform.position = Vector3.Lerp(transform.position, m_target.position, t);
@@ -60,5 +71,11 @@ public class Enemy : MonoBehaviour, IPushable
             dist = Vector3.Distance(transform.position, m_target.position);
         }
         m_movement = null;
+    }
+
+    IEnumerator DestroyAfterTimeline()
+    {
+        yield return new WaitForSeconds((float)playableDirector.duration);
+        Destroy(gameObject);
     }
 }
